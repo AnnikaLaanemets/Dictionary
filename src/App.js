5a130e2e8c06d4b9a609b2978b5bf579bdf8cab3
AnnikaLaa <annika0096@gmail.com>
2023-06-22 18:54:08 +0300

    search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
-      <p></p>
+      <div className="container">
+        <h1>Dictionary</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Made by Annika</footer>
+      </div>
     </div>
   );
 }
